c7631838093c6ca7a201f1288742217ccddb668b
Rafa Cardenas <253999660+rafa-stacks@users.noreply.github.com>
2026-03-09 09:18:40 -0600

    fix: optimize indexes

---
+++ migrations/1773069226616_index-optimize.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export function up(pgm: MigrationBuilder): void {
+  pgm.dropIndex('metadata', ['token_id']);
+  pgm.dropIndex('rate_limited_hosts', ['hostname']);
+  pgm.dropIndex('tokens', ['smart_contract_id']);
+
+  pgm.createIndex('blocks', [{ name: 'block_height', sort: 'DESC' }], {
+    where: 'canonical = true',
+  });
+  pgm.createIndex('tokens', ['type'], { where: 'canonical = true' });
+}
+
+export function down(pgm: MigrationBuilder): void {
+  pgm.createIndex('metadata', ['token_id']);
+  pgm.createIndex('rate_limited_hosts', ['hostname']);
+  pgm.createIndex('tokens', ['smart_contract_id']);
+
+  pgm.dropIndex('blocks', [{ name: 'block_height', sort: 'DESC' }]);
+  pgm.dropIndex('tokens', ['type']);
+}
